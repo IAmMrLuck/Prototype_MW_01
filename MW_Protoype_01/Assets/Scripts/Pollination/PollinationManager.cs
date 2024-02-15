cf81fdad9b7f6199a758c90a5de5ddd6f55b366b
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2024-02-15 19:43:35 +1100

    Ready for Build

--- MW_Protoype_01/Assets/Scripts/Pollination/PollinationManager.cs
+++ MW_Protoype_01/Assets/Scripts/Pollination/PollinationManager.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class PollinationManager : MonoBehaviour
 {
+    [SerializeField] private Material petalMaterial;
+
+    private void Start()
+    {
+        petalMaterial.SetColor("_Color", Color.red);
+        // Assuming the child gameObject is named "ChildGameObject"
+        Transform child = transform.Find("Petals");
+
+        if (child != null)
+        {
+            Renderer renderer = child.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material = petalMaterial;
+            }
+            else
+            {
+                Debug.LogWarning("Child does not have a Renderer component.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Child GameObject not found.");
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Bee In Trigger");
+        Debug.Log("in Trigger");
+        petalMaterial.SetColor("_Color", Color.blue);
     }
 }
